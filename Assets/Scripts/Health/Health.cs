31b10f0a426905826437015779e4c688e0611c2b
stokuj <51967777+stokuj@users.noreply.github.com>
2022-06-23 12:53:58 +0200

    Adding immunity for short duration after taking damage and new feature of changing player color after damage( for the time he is immune to damage)

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
-{
+{   
+
+    [Header ("Health")]
     [SerializeField] private float startHealth;
     public float currentHealth { get; private set; }
     private Animator anim; 
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startHealth;
-
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -21,8 +29,10 @@ public class Health : MonoBehaviour
         if(currentHealth > 0)
         {
 
-            anim.SetTrigger("hurt");        }
+            anim.SetTrigger("hurt");        
             //ifframes
+            StartCoroutine(Invunerability());
+        }
         else
         {
             if(!dead)
@@ -37,5 +47,19 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startHealth);
+    }
+
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
